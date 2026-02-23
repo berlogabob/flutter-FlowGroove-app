c764a1f1e1704d3bef2a887b22adb162db814547
BerlogaBob <berloga.bob@gmail.com>
2026-02-23 17:44:39 +0000

    Release 0.11.2+33

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1779889223" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "143747726" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
