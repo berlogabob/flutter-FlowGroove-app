e176ab25b3f7d9d3806d3c65e031cc774554363a
BerlogaBob <berloga.bob@gmail.com>
2026-02-28 10:40:28 +0000

    new tools screen. start

---
+++ lib/widgets/tools/tools.dart
@@ -0,0 +1,7 @@
+// Tools - Shared Widgets
+// Reusable components for all tool screens (Metronome, Tuner, etc.)
+
+export 'tool_scaffold.dart';
+export 'tool_app_bar.dart';
+export 'tool_transport_bar.dart';
+export 'tool_mode_switcher.dart';
