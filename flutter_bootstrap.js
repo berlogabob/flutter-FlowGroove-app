2c19e9b95b4daf604a60073b09a0fd6035714c9b
BerlogaBob <berloga.bob@gmail.com>
2026-03-14 19:26:30 +0000

    Deploy 0.13.1+168 to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3434035260" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1177438475" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
