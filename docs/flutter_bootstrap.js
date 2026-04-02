7c041924abf156990b299605f0ba988066d25b79
BerlogaBob <berloga.bob@gmail.com>
2026-04-02 16:17:56 +0100

    docs: Deploy test build   "version": "0.13.4",

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2390546763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3758131964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
