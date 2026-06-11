b7bcf9a95f3eaa5f49510f47c4fce939c42bca68
BerlogaBob <berloga.bob@gmail.com>
2026-06-11 23:30:38 +0100

    deploy: hugo landing + flutter app {

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1095739031" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2450515965" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
